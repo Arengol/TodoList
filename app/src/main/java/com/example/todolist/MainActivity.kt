40869b6e5b40e7da879d2630d7f364afb1c60fa2
Vladimir Vorobev <worobev2001@gmail.com>
2022-05-14 17:57:26 +0300

    Add data class usage

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import com.example.todolist.datamodel.Keep
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val todoListView: ListView by lazy {  findViewById<ListView>(R.id.todo_list_view) }
+    private val arrayAdapter: ArrayAdapter<*> by lazy { ArrayAdapter(this, android.R.layout.simple_list_item_checked,viewInitialize()) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val arrayAdapter: ArrayAdapter<*>
-        val things = arrayOf("0","1","2","3","4")
-
-        val todoListView = findViewById<ListView>(R.id.todo_list_view)
-        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,things)
+        todoListView.choiceMode = ListView.CHOICE_MODE_MULTIPLE
         todoListView.adapter = arrayAdapter
+    }
+
+    private fun viewInitialize(): Array<Keep> {
+        val things = arrayOf(
+            Keep("Eat", Calendar.getInstance().time, false),
+            Keep("Code", Calendar.getInstance().time, false),
+            Keep("Repeat", Calendar.getInstance().time, false)
+        )
+        for (i in 0..things.size-1){
+            todoListView.setItemChecked(i, things[i].isDone)
+        }
+        return things
     }
 }

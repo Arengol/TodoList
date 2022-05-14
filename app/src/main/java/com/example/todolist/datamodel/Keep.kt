b44fe816975142070642c44bcd6d6a0b768190fb
Vladimir Vorobev <worobev2001@gmail.com>
2022-05-14 17:58:25 +0300

    Specify data class

--- app/src/main/java/com/example/todolist/datamodel/Keep.kt
+++ app/src/main/java/com/example/todolist/datamodel/Keep.kt
@@ -1,4 +1,10 @@
 package com.example.todolist.datamodel
 
-class Keep {
+import java.util.Date
+
+data class Keep(var description: String, var date: Date, var isDone: Boolean)
+{
+    override fun toString(): String {
+        return super.toString()
+    }
 }

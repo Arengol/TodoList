abb3e22cce2f2525f4c1ba6de11bc149e8af6a05
Vladimir Vorobev <worobev2001@gmail.com>
2022-05-14 16:23:14 +0300

    Test listView

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val arrayAdapter: ArrayAdapter<*>
+        val things = arrayOf("0","1","2","3","4")
+
+        val todoListView = findViewById<ListView>(R.id.todo_list_view)
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,things)
+        todoListView.adapter = arrayAdapter
     }
 }

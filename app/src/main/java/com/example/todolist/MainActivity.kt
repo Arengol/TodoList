c1f78ae631021b9ed5564a4cca54d47751c9cf08
Vladimir Vorobev <worobev2001@gmail.com>
2022-05-16 22:00:52 +0300

    Add menu buttons

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import com.example.todolist.datamodel.Task
@@ -11,12 +12,27 @@ class MainActivity : AppCompatActivity() {
 
     private val todoListView: ListView by lazy {  findViewById<ListView>(R.id.todo_list_view) }
     private val itemAdapter: TaskListAdapter by lazy { TaskListAdapter(this, viewInitialize()) }
-
+    private val menuFab: View by lazy {findViewById(R.id.fab_menu)}
+    private val deleteFab: View by lazy {findViewById(R.id.fab_delete)}
+    private val createFab: View by lazy {findViewById(R.id.fab_create)}
+    private var fabStatus = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         todoListView.choiceMode = ListView.CHOICE_MODE_MULTIPLE
         todoListView.adapter = itemAdapter
+        menuFab.setOnClickListener {
+            if (fabStatus) {
+                deleteFab.animate().translationY(-150F)
+                createFab.animate().translationY(-300F)
+                fabStatus = false
+            }
+            else {
+                deleteFab.animate().translationY(0F)
+                createFab.animate().translationY(0F)
+                fabStatus = true
+            }
+        }
     }
 
     private fun viewInitialize(): Array<Task> {

226733b4e5133d9647b1476434460eff6da1549f
Vladimir Vorobev <worobev2001@gmail.com>
2022-05-17 21:57:20 +0300

    Add data deletion

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -5,17 +5,22 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.core.view.isVisible
+import androidx.core.view.iterator
 import com.example.todolist.datamodel.Task
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
+    private val things = ArrayList<Task>()
     private val todoListView: ListView by lazy {  findViewById<ListView>(R.id.todo_list_view) }
     private val itemAdapter: TaskListAdapter by lazy { TaskListAdapter(this, viewInitialize()) }
     private val menuFab: View by lazy {findViewById(R.id.fab_menu)}
     private val deleteFab: View by lazy {findViewById(R.id.fab_delete)}
     private val createFab: View by lazy {findViewById(R.id.fab_create)}
     private var fabStatus = true
+    private var deleteTaskButtonStatus = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         todoListView.adapter = itemAdapter
         menuFab.setOnClickListener {
             if (fabStatus) {
-                deleteFab.animate().translationY(-150F)
+                deleteFab.animate().translationY(-166F)
                 createFab.animate().translationY(-300F)
                 fabStatus = false
             }
@@ -33,14 +38,24 @@ class MainActivity : AppCompatActivity() {
                 fabStatus = true
             }
         }
+        deleteFab.setOnClickListener {
+            if(deleteTaskButtonStatus){
+                for(i in todoListView)
+                    i.findViewById<View>(R.id.delete_task).isVisible=true
+                deleteTaskButtonStatus=false
+            }
+            else{
+                for(i in todoListView)
+                    i.findViewById<View>(R.id.delete_task).isVisible=false
+                deleteTaskButtonStatus=true
+            }
+        }
     }
 
-    private fun viewInitialize(): Array<Task> {
-        val things = arrayOf(
-            Task("Eat", Calendar.getInstance().time, false),
-            Task("Code", Calendar.getInstance().time, false),
-            Task("Repeat", Calendar.getInstance().time, false)
-        )
+    private fun viewInitialize(): ArrayList<Task> {
+        things.add(Task("Eat", Calendar.getInstance().time, false))
+        things.add(Task("Code", Calendar.getInstance().time, false))
+        things.add(Task("Repeat", Calendar.getInstance().time, false))
         for (i in 0..things.size-1){
             todoListView.setItemChecked(i, things[i].status)
         }
--- app/src/main/java/com/example/todolist/TaskListAdapter.kt
+++ app/src/main/java/com/example/todolist/TaskListAdapter.kt
@@ -4,11 +4,12 @@ import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
 import com.example.todolist.datamodel.Task
 
-class TaskListAdapter (private val context: Activity, private val taskArray: Array<Task>):
+class TaskListAdapter (private val context: Activity, private val taskArray: ArrayList<Task>):
     ArrayAdapter<Task>(context, R.layout.custom_list_item, taskArray){
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val inflater = context.layoutInflater
@@ -16,9 +17,14 @@ class TaskListAdapter (private val context: Activity, private val taskArray: Arr
         val task = customListItem.findViewById<TextView>(R.id.task)
         val date = customListItem.findViewById<TextView>(R.id.date)
         val status = customListItem.findViewById<CheckBox>(R.id.status)
+        val deleteButton = customListItem.findViewById<View>(R.id.delete_task)
         task.text = taskArray[position].description
         date.text = taskArray[position].getDateString()
         status.isActivated = taskArray[position].status
+        deleteButton.setOnClickListener {
+            taskArray.removeAt(position)
+            this.notifyDataSetChanged()
+        }
         return customListItem
     }
 }
